215ad1cc9a0e189e44ed573e8dad620d1f641b43
Ruslan <berkutruslan1@gmail.com>
2020-11-30 13:38:25 +0200

    added product and category models, registered in admin

--- products/models.py
+++ products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=125)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Category'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=125)
+    category = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Product'
