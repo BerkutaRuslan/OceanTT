a085b7047779b7534a25561183162d330791f573
Ruslan <berkutruslan1@gmail.com>
2020-11-30 17:46:41 +0200

    added new fields, created initial migration

--- products/models.py
+++ products/models.py
@@ -13,7 +13,9 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=125)
+    price = models.FloatField(blank=False, null=False)
     is_deleted = models.BooleanField(default=False)
+    is_published = models.BooleanField(default=True)
     category = models.ManyToManyField(Category, related_name='products')
 
     def __str__(self):
