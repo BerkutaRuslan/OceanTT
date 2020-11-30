f686ace5b858db64056aacfb35057a8552c3f883
Ruslan <berkutruslan1@gmail.com>
2020-11-30 17:48:13 +0200

    added endpoint to list all products + accepts queryparams

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from products.views import ProductCreateView, CategoryCreateView, ProductUpdateView, ProductDeleteView, \
-    DeleteCategoryView
+    DeleteCategoryView, ListProductsView
 
 urlpatterns = [
     path('create', ProductCreateView.as_view()),
     path('create/category', CategoryCreateView.as_view()),
     path('update/<int:pk>', ProductUpdateView.as_view()),
     path('delete/<int:pk>', ProductDeleteView.as_view()),
-    path('delete/category/<int:pk>', DeleteCategoryView.as_view())
+    path('delete/category/<int:pk>', DeleteCategoryView.as_view()),
+    path('list-products', ListProductsView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,5 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import generics, status
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
@@ -17,7 +19,7 @@ class CategoryCreateView(generics.CreateAPIView):
     queryset = Category.objects.all()
 
 
-class ProductUpdateView(generics.RetrieveUpdateAPIView):
+class ProductUpdateView(generics.UpdateAPIView):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
 
@@ -26,6 +28,20 @@ class ProductUpdateView(generics.RetrieveUpdateAPIView):
 
 
 class ProductDeleteView(APIView):
+
+    @swagger_auto_schema(
+        operation_id='Delete Product',
+        operation_description="Endpoint for deleting Product by id",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+
+        ),
+        responses={
+            200: 'OK',
+        },
+        security=[],
+        tags=['products'],
+    )
     def delete(self, request, pk):
         try:
             product = Product.objects.get(pk=pk)
@@ -38,6 +54,19 @@ class ProductDeleteView(APIView):
 
 
 class DeleteCategoryView(APIView):
+    @swagger_auto_schema(
+        operation_id='Delete Category',
+        operation_description="Endpoint for deleting Category by id",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+
+        ),
+        responses={
+            200: 'OK',
+        },
+        security=[],
+        tags=['category'],
+    )
     def delete(self, request, pk):
         try:
             category = Category.objects.get(pk=pk)
@@ -51,3 +80,36 @@ class DeleteCategoryView(APIView):
         except Category.DoesNotExist:
             msg = 'Category does not exists'
             raise ValidationError(msg)
+
+
+class ListProductsView(generics.ListAPIView):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        products = Product.objects.all()
+        name = self.request.GET.get("name")
+        category_id = self.request.GET.get('category_id')
+        is_published = self.request.GET.get('is_published')
+        sort_min_max = self.request.GET.get('sort_min_max')
+        sort_max_min = self.request.GET.get('sort_max_min')
+        if name:
+            products = products.filter(name=name)
+        if category_id:
+            products = products.filter(category=category_id)
+        if sort_min_max:
+            products = products.order_by('price')
+        if sort_max_min:
+            products = products.order_by('-price')
+        if is_published:
+            products = products.filter(is_published=is_published)
+        return products.filter(is_deleted=False)
+
+    @swagger_auto_schema()
+    def get(self, request, *args, **kwargs):
+        """
+        Url for listing info about products, accepts query params like:
+        name= , category_id= , is_published= , sort_min_max = True/False,  sort_max_min = True/False,
+        is_published = True/False.
+        Example url: http://127.0.0.1:8000/products/list-products?category_id=3&name=Pizza&is_published=True&sort_min_max=True
+        """
+        return super().get(request, args, kwargs)
